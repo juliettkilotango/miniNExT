f75b97be0d88c19bc3aa2a70d813bd00f74b971e
vikas89 <vikas89@gmail.com>
2014-12-31 01:36:27 -0800

    adding new bird example

---
+++ examples/bird_exp_2/start.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+"""
+Example network of Bird routers
+(BirdTopo + BirdService)
+"""
+
+import sys
+import atexit
+
+# patch isShellBuiltin
+import mininet.util
+import mininext.util
+mininet.util.isShellBuiltin = mininext.util.isShellBuiltin
+sys.modules['mininet.util'] = mininet.util
+
+from mininet.util import dumpNodeConnections
+from mininet.node import OVSController
+from mininet.log import setLogLevel, info
+
+from mininext.cli import CLI
+from mininext.net import MiniNExT
+
+from topo import BirdTopo
+
+net = None
+
+
+def startNetwork():
+    "instantiates a topo, then starts the network and prints debug information"
+
+    info('** Creating Bird network topology\n')
+    topo = BirdTopo()
+
+    info('** Starting the network\n')
+    global net
+    net = MiniNExT(topo, controller=OVSController)
+    net.start()
+
+    info('** Dumping host connections\n')
+    dumpNodeConnections(net.hosts)
+
+    info('** Testing network connectivity\n')
+    net.ping(net.hosts)
+
+    info('** Dumping host processes\n')
+    for host in net.hosts:
+        host.cmdPrint("ps aux")
+	
+	if host.name == "rs":
+		host.cmd('ifconfig rs-eth1 192.168.1.4 netmask 255.255.255.0')
+		host.cmd('sysctl -w net.ipv4.ip_forward=1')
+		host.cmd('sysctl -p /etc/sysctl.conf')
+		host.cmd('/etc/init.d/procps restart')
+
+    info('** Running CLI\n')
+    CLI(net)
+
+
+def stopNetwork():
+    "stops a network (only called on a forced cleanup)"
+
+    if net is not None:
+        info('** Tearing down Bird network\n')
+        net.stop()
+
+if __name__ == '__main__':
+    # Force cleanup on exit by registering a cleanup function
+    atexit.register(stopNetwork)
+
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    startNetwork()
